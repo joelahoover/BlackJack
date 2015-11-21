474d33ea76653c47166036ef07be547e18d4c1be
Kyle Easop <kyleeasop@Kyles-MacBook-Pro.local>
2015-11-21 00:37:21 -0500

    I began to add some dummy images and get started on basic animations. I'm still having a bit of hold ups, so we'll see how far I can get.

--- BlckJck/ViewController.mm
+++ BlckJck/ViewController.mm
@@ -19,6 +19,12 @@
 }
 @property (weak, nonatomic) IBOutlet UILabel *currentHandLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalHandLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *cardBack;
+@property (weak, nonatomic) IBOutlet UIImageView *cardFront;
+@property (weak, nonatomic) IBOutlet UIImageView *duckWing;
+@property (weak, nonatomic) IBOutlet UIButton *hitButton;
+@property (weak, nonatomic) IBOutlet UIButton *holdButton;
+
 
 @end
 
@@ -26,7 +32,61 @@
 
 - (IBAction)hitMeButton:(id)sender {
     playerHand.Hit(deck.dealCard());
-    _currentHandLabel.text = [NSString stringWithCString:playerHand.to_string().c_str() encoding:[NSString defaultCStringEncoding]];
+//    _currentHandLabel.text = [NSString stringWithCString:playerHand.to_string().c_str() encoding:[NSString defaultCStringEncoding]];
+    
+    //Disable additional button input while animation is running.
+    self.hitButton.enabled = false;
+    self.holdButton.enabled = false;
+    
+    //Rotate the duck flipper
+    [UIView animateWithDuration:1.1
+                          delay:0.0
+                        options:(UIViewAnimationCurveEaseInOut)
+                     animations:^{
+                         [UIView setAnimationDelegate:self];
+                         
+                         self.duckWing.transform = CGAffineTransformMakeRotation(M_PI/-2);
+                         //                         self.cardBack.alpha = 0;
+                     }
+                     completion:^(BOOL finished) {
+                         [UIView animateWithDuration:1.5
+                                               delay:0.0
+                                             options:(UIViewAnimationCurveEaseInOut)
+                                          animations:^{
+                                              [UIView setAnimationDelegate:self];
+                                              
+                                              self.duckWing.transform = CGAffineTransformMakeRotation(M_PI);
+                                              //                         self.cardBack.alpha = 0;
+                                          }
+                                           completion:nil];
+                          }
+     
+     ];
+    
+    //Move card from the deck to the middle of the view
+    [UIView animateWithDuration:1.1
+                          delay:0.0
+                        options:(UIViewAnimationCurveEaseInOut)
+                     animations:^{
+                         [UIView setAnimationDelegate:self];
+                         
+                         self.cardBack.center  = CGPointMake(self.view.bounds.size.width/2, self.view.bounds.size.height/2);
+                         //                         self.cardBack.alpha = 0;
+                     }
+                     completion:^(BOOL finished) {
+                         [UIView animateWithDuration:1.0
+                                          animations:^{
+                             self.cardBack.frame = CGRectMake(self.view.bounds.size.width/2 - 25, self.view.bounds.size.height/2 - 40, 50, 80);
+                         }];
+                     }
+     
+     ];
+
+    
+    
+    //Allow the buttons to be hit again.
+    self.hitButton.enabled = true;
+    self.holdButton.enabled = true;
 }
 
 - (IBAction)holdButton:(id)sender {
